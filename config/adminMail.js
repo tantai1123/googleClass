beb666f511154b175d61c9809c443936d8358ee9
Cuong <phamquoccuong2k@gmail.com>
2020-04-26 13:21:05 +0700

    update

--- config/adminMail.js
+++ config/adminMail.js
@@ -1,4 +1,6 @@
 module.exports = {
-    user: 'tantai080998@gmail.com',
-    pass: 'tai4470113'
+    // user: 'tantai080998@gmail.com',
+    // pass: 'tai4470113'
+    user: 'classroomfptgreenwich@gmail.com',
+    pass: 'Classroom123'
 }
